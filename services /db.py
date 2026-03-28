05078e63a6971ee73d8247bce5930d12a986fc55
Szedor <seugendoru@yahoo.com>
2026-03-28 17:05:01 +0200

    Delete services /db.py

--- services /db.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-from supabase import create_client
-from config import Config
-
-
-@st.cache_resource
-def get_supabase():
-    return create_client(Config.SUPABASE_URL, Config.SUPABASE_KEY)
