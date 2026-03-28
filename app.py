545b6660daf820a7f600c56012f5bb471690a0cc
Szedor <seugendoru@yahoo.com>
2026-03-28 14:37:40 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 import streamlit as st
 
-from config import Config
-
 import calea1_explorator
 import calea2_admin
 import calea3_brainstorming
@@ -13,12 +11,7 @@ st.set_page_config(page_title="Interfata IDBDC", layout="wide")
 query_params = st.query_params
 calea_activa = query_params.get("pagina", "explorator")
 
-# Validare pagină
-
-if calea_activa not in Config.VALID_PAGES:
-calea_activa = "explorator"
-
-# Router
+# Router (fără validare momentan)
 
 if calea_activa == "explorator":
 calea1_explorator.main()
@@ -28,3 +21,6 @@ calea2_admin.main()
 
 elif calea_activa == "brainstorming":
 calea3_brainstorming.main()
+
+else:
+calea1_explorator.main()
