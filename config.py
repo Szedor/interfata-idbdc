397d527f89c4088a5e794dc901e6ac5d95bf21bd
Szedor <seugendoru@yahoo.com>
2026-03-28 16:30:23 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,7 @@
 import streamlit as st
 
 class Config:
-SUPABASE_URL = st.secrets["SUPABASE_URL"]
-SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
-MAINTENANCE_PASSWORD = st.secrets["MAINTENANCE_PASSWORD"]
-
-```
-VALID_PAGES = {
-    "explorator",
-    "admin",
-    "brainstorming",
-}
-```
+    SUPABASE_URL = st.secrets["SUPABASE_URL"]
+    SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
+    MAINTENANCE_PASSWORD = st.secrets["MAINTENANCE_PASSWORD"]
+    VALID_PAGES = {"explorator", "admin", "brainstorming"}
