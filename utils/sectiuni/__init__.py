bcfa969a06f4c421fe42a1139092c0c6517569a8
Szedor <seugendoru@yahoo.com>
2026-04-23 10:34:34 +0300

    Create __init__.py

---
+++ utils/sectiuni/__init__.py
@@ -0,0 +1,9 @@
+# =========================================================
+# utils/sectiuni/__init__.py
+# v.modul.1.0 - Exportă funcțiile din toate secțiunile
+# =========================================================
+
+from utils.sectiuni.date_baza import render_date_de_baza
+from utils.sectiuni.date_financiare import render_date_financiare
+from utils.sectiuni.echipa import render_echipa
+from utils.sectiuni.aspecte_tehnice import render_aspecte_tehnice
