cbb8283d2e4cf9a55b8a34cb96bfe6abcb7f91a6
Szedor <seugendoru@yahoo.com>
2026-04-30 01:00:28 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,7 @@
-# =========================================================
-# IDBDC/admin/ui.py
-# VERSIUNE: 5.1
-# STATUS: in adaptare
-# DATA: 2026.04.30
-# =========================================================
-
 import streamlit as st
 
 class Config:
-    try:
-        SUPABASE_URL = st.secrets["SUPABASE_URL"]
-        SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
-        MAINTENANCE_PASSWORD = st.secrets["MAINTENANCE_PASSWORD"]
-        VALID_PAGES = {"explorator", "admin", "brainstorming"}
+    SUPABASE_URL = st.secrets["SUPABASE_URL"]
+    SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
+    MAINTENANCE_PASSWORD = st.secrets["MAINTENANCE_PASSWORD"]
+    VALID_PAGES = {"explorator", "admin", "brainstorming"}
