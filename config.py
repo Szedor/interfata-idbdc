cb9d0d4868afa09637acc0b82b7a6435426a38d7
Szedor <seugendoru@yahoo.com>
2026-04-30 00:54:58 +0300

    Update config.py

--- config.py
+++ config.py
@@ -12,8 +12,4 @@ class Config:
         SUPABASE_URL = st.secrets["SUPABASE_URL"]
         SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
         MAINTENANCE_PASSWORD = st.secrets["MAINTENANCE_PASSWORD"]
-    except:
-        SUPABASE_URL = ""
-        SUPABASE_KEY = ""
-        MAINTENANCE_PASSWORD = ""
-    VALID_PAGES = {"explorator", "admin", "brainstorming"}
+        VALID_PAGES = {"explorator", "admin", "brainstorming"}
