f71dce7ba6cc1b7ac0b0e3cc5c9ea49abbd314ff
Szedor <seugendoru@yahoo.com>
2026-05-28 23:12:02 +0300

    Update __init__.py

--- domenii/_baza/__init__.py
+++ domenii/_baza/__init__.py
@@ -1,1 +1,5 @@
-
+# =========================================================
+# IDBDC/domenii/_baza/__init__.py
+# v.modul.1.0 - Marchează directorul ca modul Python
+# =========================================================
+# Acest fișier poate rămâne gol.
