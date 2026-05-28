64c033c157400bc9d7eb67f3684e69f4542edeb8
Szedor <seugendoru@yahoo.com>
2026-05-28 20:31:26 +0300

    Create __init__.py

---
+++ domenii/_baza/sectiune_financiar/__init__.py
@@ -0,0 +1,12 @@
+# =========================================================
+# domenii/_baza/sectiune_financiar/__init__.py
+# v.modul.1.0 - Exportă funcțiile specifice
+# =========================================================
+
+from domenii._baza.sectiune_financiar.contracte_cep import render as render_contracte_cep
+from domenii._baza.sectiune_financiar.contracte_terti import render as render_contracte_terti
+from domenii._baza.sectiune_financiar.contracte_speciale import render as render_contracte_speciale
+from domenii._baza.sectiune_financiar.proiecte_fdi import render as render_proiecte_fdi
+from domenii._baza.sectiune_financiar.proiecte_internationale import render as render_proiecte_internationale
+from domenii._baza.sectiune_financiar.proiecte_pncdi import render as render_proiecte_pncdi
+from domenii._baza.sectiune_financiar.proiecte_pnrr import render as render_proiecte_pnrr
