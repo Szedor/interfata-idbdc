cd20bc664cf5a7206b41933dad88bf51afcebfc0
Szedor <seugendoru@yahoo.com>
2026-06-03 17:32:42 +0300

    Update display_rendering.py

--- utils/display_rendering.py
+++ utils/display_rendering.py
@@ -42,6 +42,18 @@ _COL_ORDER_FDI = [
     "status_contract_proiect", "program", "cod_domeniu_fdi", "cod_temporar",
 ]
 
+_COL_ORDER_PNCDI = [
+    "denumire_categorie", "acronim_tip_proiecte", "cod_identificare",
+    "data_contract", "titlul_proiect", "acronim_proiect",
+    "domeniu_cercetare",
+    "data_inceput", "data_sfarsit", "durata",
+    "status_contract_proiect",
+    "numar_participanti", "denumire_participanti",
+    "rol_upt", "identificare_apel", "data_inchidere_apel",
+    "programul", "subprogramul", "instrument_finantare",
+    "website",
+]
+
 _COL_ORDER_STRUCTURALE = [
     "denumire_categorie", "acronim_tip_proiecte", "cod_identificare",
     "titlul_proiect", "acronim_proiect",
@@ -112,6 +124,7 @@ _COL_ORDER_PROIECTE_GENERIC = [
 # Mapare tabelă → ordine specifică
 _COL_ORDER_PER_TABLE = {
     "base_proiecte_fdi":            _COL_ORDER_FDI,
+    "base_proiecte_pncdi":          _COL_ORDER_PNCDI,
     "base_proiecte_structurale":    _COL_ORDER_STRUCTURALE,
     "base_proiecte_nonue":          _COL_ORDER_NONUE,
     "base_proiecte_see":            _COL_ORDER_SEE,
@@ -148,6 +161,10 @@ _COL_ORDER_FINANCIAR = [
     # FDI
     "suma_solicitata_fdi", "suma_aprobata_mec",
     "cofinantare_upt_fdi", "total_buget_proiect_fdi",
+    # PNCDI (com_date_financiare_pn)
+    "an_referinta", "valoare_contract_an_referinta",
+    "cofinantare_contract_an_referinta",
+    "valoare_totala_contract", "cofinantare_totala_contract",
     # STRUCTURALE
     "cheltuieli_neeligibile", "costuri_totale_upt",
     # NONUE
